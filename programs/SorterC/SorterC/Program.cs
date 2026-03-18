ddca36d699d4581b729e343c2d30a6dfd942b219
Khymari Sandy <khymari.sandy@dawsoncollege.qc.ca>
2026-03-18 10:40:40 -0400

    Add sort proj

---
+++ programs/SorterC/SorterC/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> list = new List<int>();
+
+var r = new Random();
+
+for (int i = 0; i < 10; i++)
+    list.Add(r.Next(0, 100) + 1);
+
+Console.WriteLine($"Unsorted: {string.Join(",", list)}");
+list.Sort();
+Console.WriteLine($"Sorted: {string.Join(",", list)}");
